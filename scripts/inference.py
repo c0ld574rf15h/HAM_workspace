440256acc012bd02cb584635dcb6a29591eb1831
c0ld574rf15h <c0ld574rf15h@gmail.com>
2020-06-28 23:39:49 +0000

    Update inference code to save results

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,4 +1,4 @@
-import sys
+import sys, pickle
 sys.path.append('/workspace/modules')
 
 from tqdm import tqdm
@@ -24,7 +24,8 @@ def fetch_dataset():
 
 if __name__ == '__main__':
     config = read_config('test')
-    hit = 0
+    hit, all_preds = 0, []
+    #flow_embeddings = np.zeros((10000, 256))
 
     model = HierarchicalAttentionModel().to('cuda')
     model.load_state_dict(
@@ -41,11 +42,27 @@ if __name__ == '__main__':
 
     log.info('Running inference...')
     with torch.no_grad():
-        for batch_id, (test_X, test_y) in enumerate(test_dataloader):
-            preds = model(test_X.type(torch.long).to('cuda'))
-            loss = criterion(preds, test_y.type(torch.long).to('cuda'))
+        num_iteration = len(test_dataloader) // 128
+        with tqdm(total=num_iteration) as pbar:
+            for batch_id, (test_X, test_y) in enumerate(test_dataloader):
+                preds, flow_embedding = model(test_X.type(torch.long).to('cuda'))
+                loss = criterion(preds, test_y.type(torch.long).to('cuda'))
 
-            hit += (preds.argmax(dim=1) == test_y.type(torch.long).to('cuda')).sum().item()
+                hit += (preds.argmax(dim=1) == test_y.type(torch.long).to('cuda')).sum().item()
+                all_preds += [i.item() for i in preds.argmax(dim=1)]
+                #flow_embeddings[batch_id*128:batch_id*128+flow_embedding.shape[0],] = flow_embedding.data.cpu().numpy()
+
+                pbar.update(1)
 
     accuracy = 100 * (hit / len(test_dataset))
     log.info('Accuracy : {:.2f}'.format(accuracy))
+
+    #np.save(wrap_path('results/flow_embedding'), flow_embeddings)
+
+    with open(
+        wrap_path(
+            config['PredictionSavePath'].format(
+                epoch=config['Checkpoint']
+            )
+        ), 'wb') as f:
+        pickle.dump(all_preds, f)
